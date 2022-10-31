04314659d7a86456b2aed9b877f916b45f73a5b6
tardeepsingh <116921610+tardeepsingh@users.noreply.github.com>
2022-10-31 15:52:06 -0600

    Create Data.sql

---
+++ Clothing Store Database/Data.sql
@@ -0,0 +1,18 @@
+# Insert data into the table
+
+INSERT into JKF VALUES 
+(1, "Shirt", 50, "Men", "M", "Yes"),
+(2, "Jean", 80, "Women", "S", "Yes"), 
+(3, "Skirt", 45, "Baby", "S", "No"), 
+(4, "T-Shirt", 40, "Women", "L", "Yes"),
+(5, "Joggers", 85, "Men", "S", "Yes"),
+(6, "Polo", 60, "Men", "XL", "Yes"),
+(7, "Shorts", 85, "Men", "S", "No"),
+(8, "Pajama", 100, "Baby", "XXXS", "Yes"),
+(9, "Hoodie", 160, "Women", "L", "Yes"),
+(10, "Sweaters", 109, "Men", "M", "No"),
+(11, "Dress", 75, "Women", "XXS", "Yes"),
+(12, "Outerwear", 218, "Women", "XL", "Yes"),
+(13, "Top", 96, "Women", "XS", "No"),
+(14, "Jacket", 350, "Men", "L", "Yes"),
+(15, "Socks", 60, "Men", "S", "Yes");
