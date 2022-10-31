d354e287a3371ae2a7c0c5710cdf7cc6d4c5e537
tardeepsingh <116921610+tardeepsingh@users.noreply.github.com>
2022-10-31 15:50:52 -0600

    Create Create Table.sql

---
+++ Clothing Store Database/Create Table.sql
@@ -0,0 +1,10 @@
+-- Create a clothing store database
+
+CREATE table JKF 
+(id integer PRIMARY KEY, 
+item text, 
+item_price integer, 
+item_section text, 
+item_size text, 
+item_availability text);
+
