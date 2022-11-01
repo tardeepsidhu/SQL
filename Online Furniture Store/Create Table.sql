9797ba929525b05d238bc2326cd2fb4f68c76aff
tardeepsingh <116921610+tardeepsingh@users.noreply.github.com>
2022-11-01 10:34:39 -0600

    Create Create Table.sql

---
+++ Online Furniture Store/Create Table.sql
@@ -0,0 +1,20 @@
+/*
+Sales from an online furniture store
+Collected by: https://www.khanacademy.org/profile/charlesb2000/
+*/
+
+CREATE TABLE sales(
+  ID INTEGER  NOT NULL PRIMARY KEY
+, transaction_date TEXT
+, product TEXT
+, price INTEGER
+, payment_type TEXT
+, name TEXT
+, city TEXT
+, state TEXT
+, country TEXT
+, account_created TEXT
+, last_login TEXT
+, latitude REAL
+, longitude REAL
+);
