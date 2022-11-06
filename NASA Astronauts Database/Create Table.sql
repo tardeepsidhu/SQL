6b1a673b25646e10eedb23edbb1bf913ef704637
tardeepsingh <116921610+tardeepsingh@users.noreply.github.com>
2022-11-06 13:19:12 -0700

    Create Create Table.sql

---
+++ NASA Astronauts Database/Create Table.sql
@@ -0,0 +1,24 @@
+-- NASA Astronauts, 1959-Present
+-- Source: https://www.kaggle.com/nasa/astronaut-yearbook
+
+CREATE TABLE astronauts(
+   Name                TEXT PRIMARY KEY,
+   Year                INTEGER,
+   GroupNum            INTEGER,
+   Status              TEXT,
+   Birth_Date          TEXT,
+   Birth_Place         TEXT,
+   Gender              TEXT,
+   Alma_Mater          TEXT,
+   Undergraduate_Major TEXT,
+   Graduate_Major      TEXT,
+   Military_Rank       TEXT,
+   Military_Branch     TEXT,
+   Space_Flights       INTEGER,
+   Space_Flight_hr     INTEGER,
+   Space_Walks         INTEGER,
+   Space_Walks_hr      REAL,
+   Missions            TEXT,
+   Death_Date          TEXT, 
+   Death_Mission       TEXT
+);
